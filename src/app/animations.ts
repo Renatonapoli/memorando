cbca4a48992c67781db6e8844df6b8534d33e7d6
Renato-napoli <renato.napoli75@gmail.com>
2024-11-23 18:30:37 -0300

    Animando o input do formulario e corrigindo erros no console

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -124,7 +124,17 @@ export const mensagemListaVaziaTrigger = trigger('mensagemListaVazia', [
 export const shakeTrigger = trigger('shakeAnimation', [
   transition('* => *', [
     query('input.ng-invalid:focus, select.ng-invalid:focus', [
-      animate('0.5s', style({ border: '4px solid red'}))
-    ])
+      animate('0.5s', keyframes([
+        style({ border: '2px solid red'}),
+        style({ transform: 'translateX(0)'}),
+        style({ transform: 'translateX(-10px)'}),
+        style({ transform: 'translateX(10px)'}),
+        style({ transform: 'translateX(-10px)'}),
+        style({ transform: 'translateX(10px)'}),
+        style({ transform: 'translateX(-10px)'}),
+        style({ transform: 'translateX(10px)'}),
+        style({ transform: 'translateX(0)'}),
+      ]))
+    ], {optional: true})
   ])
 ])
