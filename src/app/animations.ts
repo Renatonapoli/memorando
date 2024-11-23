bc4b01a6fd2e54054c081295cada8cbff193db24
Renato-napoli <renato.napoli75@gmail.com>
2024-11-23 18:05:56 -0300

    Metodo query

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, query, state, style, transition, trigger } from '@angular/animations';
 
 export const highlightedStateTrigger = trigger('highlightedState', [
   state('default', style({
@@ -60,7 +60,8 @@ export const filterTrigger = trigger('filterAnimation', [
 
 export const formButtonTrigger = trigger('formButton', [
   transition('invalid => valid', [
-    group([
+    query('#botao-salvar', [
+      group([
       animate(200, style({
         backgroundColor: '#63B77C'
       })),
@@ -69,20 +70,53 @@ export const formButtonTrigger = trigger('formButton', [
       }))
     ]),
     animate(200, style({
-      transform: 'scale(1)'
-    }))
+        transform: 'scale(1)'
+      }))
+    ]),
   ]),
   transition('valid => invalid', [
-    group([
+    query('#botao-salvar', [
+      group([
       animate(200, style({
         backgroundColor: '#6C757D'
       })),
       animate(100, style({
-        transform: 'scale(1.1)'
+          transform: 'scale(1.1)'
+        }))
+      ]),
+      animate(200, style({
+        transform: 'scale(1)'
       }))
-    ]),
-    animate(200, style({
-      transform: 'scale(1)'
-    }))
+    ])
+  ])
+])
+
+export const mensagemListaVaziaTrigger = trigger('mensagemListaVazia', [
+  transition(':enter', [
+    style({
+      width: '100%',
+      transform: 'translateX(-100%)',
+      opacity: 0
+    }),
+    group([
+      animate('0.3s 0.1s ease', style({
+        transform: 'translateX(0)',
+        width: '*'
+      })),
+      animate('0.3s ease', style({
+        opacity: 1
+      }))
+    ])
+  ]),
+  transition(':leave', [
+    group([
+      animate('0.3s ease', style({
+        transform: 'translateX(100%)',
+        width: '*'
+      })),
+      animate('0.3s 0.2s ease', style({
+        opacity: 0
+      }))
+    ])
   ])
 ])
